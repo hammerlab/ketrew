89cf7c8db278faa262f98b8654156c15e42bba1e
Sebastien Mondet <seb@mondet.org>
2014-05-29 18:06:33 -0400

    Start adding empty tasks

--- src/lib/ketrew.ml
+++ src/lib/ketrew.ml
@@ -11,7 +11,10 @@ end
 
 type value
 type volume
-type metadata
+module Metadata = struct
+  type t = string
+  let empty = ""
+end
 type dependency
 type job = <
   kind: string;
@@ -20,35 +23,69 @@ type process = [
   | `Get_command_output of string
   | `Run_job of job
 ]
-type value_type = [`String | `Number]
 type target_value = [ `String of string | `Number of float ]
 
-type target = [
-  | `Tree of File_tree.t
-  | `File of File_tree.file
-  | `Value of value_type
-]
-type running_state = {
-  identification: string; (* job id ? *)
-  stdout: string option;
-  stderr: string option;
-}
-type workflow_state = [
-  | `Submitted
-  | `Started of running_state
-  | `Dead of running_state
-  | `Successful of target_value
-]
-type workflow = {
-  id: Unique_id.t;
-  name: string;
-  persistance: [ `Input_data | `Recomputable of float | `Result ];
-  metadata: metadata;
-  dependencies: dependency list;
-  make: process;
-  target: target;
-  mutable history: workflow_state list;
-}
+module Data = struct
+
+  type 'a pointer = { id: Unique_id.t }
+  let pointer id = {id}
+
+  type value_type = [`String | `Number]
+end
+module Process = struct
+
+  type t = [ `Nop ]
+  let nop = `Nop
+end
+module Target = struct
+  type t = [
+    | `Tree of File_tree.t
+    | `File of File_tree.file
+    | `Value of Data.value_type
+  ]
+  let value vt : t = `Value vt
+  let string_value : t = `Value `String
+end
+
+module Task = struct
+
+  type running_state = {
+    identification: string; (* job id ? *)
+    stdout: string option;
+    stderr: string option;
+  }
+  type workflow_state = [
+    | `Submitted
+    | `Started of running_state
+    | `Dead of running_state
+    | `Successful of target_value
+  ]
+  type t = {
+    id: Unique_id.t;
+    name: string;
+    persistance: [ `Input_data | `Recomputable of float | `Result ];
+    metadata: Metadata.t;
+    dependencies: t Data.pointer list;
+    make: Process.t;
+    target: Target.t;
+    history: workflow_state list;
+  }
+  let create
+      ?name ?(persistance=`Input_data) ?(metadata=Metadata.empty)
+      ?(dependencies=[]) ?(make= Process.nop) ~target
+      ?(history=[])
+      id = 
+    { id; name = Option.value name ~default:id; persistance; metadata;
+      dependencies; make; target; history }
+
+  let pointer t : t Data.pointer = Data.pointer t.id
+  let serialize t = Marshal.to_string t []
+  let deserialize s : (t, _) Result.t =
+    let open Result in
+    try return (Marshal.from_string s 0)
+    with e -> fail (`Task (`Deserilization (Printexc.to_string e)))
+
+end
 
 module Database = struct
 
@@ -110,14 +147,17 @@ end
 
 module Engine = struct
   type t = {
-    current_workflows: Unique_id.t list;
+    current_tasks: Unique_id.t list;
+    (* keep db id of a list of all "archived" tasks *)
   }
-  let create () = {current_workflows = [];}
+  let create () = {current_tasks = [];}
 
   let serialize t = Marshal.to_string t []
   let unserialize s =
     try return (Marshal.from_string s 0 : t)
     with e -> fail (`Engine (`Deserilization (Printexc.to_string e)))
+  let add t task = { current_tasks = task.Task.id :: t.current_tasks }
+  let current_tasks t = t.current_tasks
 end
 
 module Configuration = struct
@@ -166,6 +206,7 @@ module State = struct
         let e = Engine.create () in
         return e
     end
+
   let save_engine t engine =
     database t >>= fun db ->
     let key = t.configuration.Configuration.engine_key in
@@ -174,6 +215,40 @@ module State = struct
       >>= function
       | `Done -> return ()
       | `Not_done -> fail (`State (`Database_unavailable key))
+    end
+
+  let add_task t task_fun =
+    let id = Unique_id.create () in
+    let actual_task = task_fun id in
+    database t
+    >>= fun db ->
+    begin Database.(act db (set id Task.(serialize actual_task)))
+      >>= function
+      | `Done -> return ()
+      | `Not_done ->
+        (* TODO: loop instead of error *) fail (`State (`Database_unavailable id))
+    end
+    >>= fun () ->
+    get_engine t
+    >>= fun engine ->
+    let new_engine = Engine.add engine actual_task in
+    save_engine t new_engine (* TODO: remove task if this fails *)
+
+  let current_tasks t =
+    database t >>= fun db ->
+    get_engine t >>= fun engine ->
+    let pointers = Engine.current_tasks engine in
+    Pvem_lwt_unix.Deferred_list.for_concurrent pointers ~f:(fun task_id ->
+        Database.get db task_id
+        >>= function
+        | Some t -> of_result (Task.deserialize t)
+        | None ->
+          fail (`State (`Database_unavailable (fmt "Task missing: %s" task_id)))
+      )
+    >>= fun (tasks, errors) ->
+    begin match errors with
+    | [] -> return tasks
+    | some :: more -> fail some (* TODO do not forget other errors *)
     end
 
 
