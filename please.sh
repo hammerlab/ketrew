f8a2602da7d47e16bac5cbb6916eae69ec25f78a
Sebastien Mondet <seb@mondet.org>
2014-06-16 18:31:37 -0400

    ci: setup travis build

--- please.sh
+++ please.sh
@@ -272,6 +272,40 @@ opam_package () {
 
 }
 
+do_travis() {
+    # install Ubuntu packages
+    case "$OCAML_VERSION,$OPAM_VERSION" in
+        3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
+        3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+        4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+        4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+        4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+        4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+        *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+    esac
+
+    sudo add-apt-repository -y ppa:$ppa
+    sudo apt-get update -qq
+    sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra aspcud opam
+
+    # configure and view settings
+    export OPAMYES=1
+    ocaml -version
+    opam --version
+    opam --git-version
+
+    # install OCaml packages
+    opam init
+    eval `opam config env`
+
+    get_dependencies
+
+    setup
+    ocp-build build
+
+
+}
+
 usage () {
     echo "usage: $0"
     echo "       $0 {setup,build,clean,doc,top,sig,get-dependencies,local-opam,opam,meta-file,install,uninstall,help}"
