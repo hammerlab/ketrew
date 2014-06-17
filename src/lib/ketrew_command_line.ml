f4d19978f62d263e3fb026b40ef2fbf4e7bb91f9
Sebastien Mondet <seb@mondet.org>
2014-06-17 15:05:48 -0400

    lib: add the `loop` run method (`Command_line`)

--- src/lib/ketrew_command_line.ml
+++ src/lib/ketrew_command_line.ml
@@ -22,6 +22,29 @@ module Return_code = struct
 
 end
 
+(** [with_cbreak f] calls with the terminal in “get key” mode. 
+         It comes from
+         http://pleac.sourceforge.net/pleac_ocaml/userinterfaces.html
+*)
+let with_cbreak (f: unit -> (_, _) t) =
+  let open Lwt_unix in
+  Lwt.(tcgetattr stdin
+       >>= fun term_init ->
+       let term_cbreak = { term_init with c_icanon = false } in
+       tcsetattr stdin TCSANOW term_cbreak
+       >>= fun () ->
+       catch f (fun exn -> return (`Error (`Failure "with_cbreak")))
+       >>= fun res ->
+       tcsetattr stdin TCSADRAIN term_init
+       >>= fun () ->
+       return res)
+
+let get_key () =
+  with_cbreak (fun () ->
+      wrap_deferred (fun () -> Lwt_io.read_char Lwt_io.stdin)
+        ~on_exn:(fun e -> (`Failure "get_key")))
+
+
 let default_item_format = 
   "- $id:$n  Name: $name → $status\n\
   \  Deps: $dependencies$n"
@@ -64,7 +87,7 @@ let log_list ~empty l =
   | more ->
     n % indent (separate n (List.map more ~f:(fun item -> s "- " % item)))
 
-let run_state ~state ~how =
+let run_state ~state ~max_sleep ~how =
   let log_happening ~what_happened =
     let open Log in
     let step_count = List.length what_happened in
@@ -86,54 +109,85 @@ let run_state ~state ~how =
          @ normal); 
     return ()
   in
+  let rec fix_point ~count history =
+    Ketrew_state.step state
+    >>= fun what_happened ->
+    let count = count + 1 in
+    begin match history with
+    | _ when what_happened = [] ->
+      return (count, what_happened :: history)
+    | previous :: _ when previous = what_happened ->
+      return (count, what_happened :: history)
+    | _ -> fix_point ~count (what_happened :: history)
+    end
+  in
   begin match how with
   | ["step"] ->
     Ketrew_state.step state
     >>= fun what_happened ->
     log_happening ~what_happened:[what_happened]
   | ["fix"] ->
-    let rec fix_point ~count history =
-      Ketrew_state.step state
-      >>= fun what_happened ->
-      let count = count + 1 in
-      begin match history with
-      | _ when what_happened = [] ->
-        return (count, what_happened :: history)
-      | previous :: _ when previous = what_happened ->
-        return (count, what_happened :: history)
-      | _ -> fix_point ~count (what_happened :: history)
-      end
-    in
     fix_point 0 []
     >>= fun (step_count, what_happened) ->
     log_happening ~what_happened:List.(rev what_happened)
+  | ["loop"] ->
+    let keep_going = ref true in
+    let traffic_light = Light.create () in
+    let rec loop previous_sleep happenings =
+      fix_point 0 []
+      >>= fun (step_count, what_happened) ->
+      let new_happenings = what_happened @ happenings in
+      let seconds =
+        match what_happened with
+        | [] | [[]] -> 
+          min (previous_sleep *. 2.) max_sleep
+        | something -> 2.
+      in
+      Log.(s "Sleeping " % f seconds % s " s" @ very_verbose);
+      log_happening ~what_happened:what_happened
+      >>= fun () ->
+      begin Deferred_list.pick_and_cancel [
+          System.sleep seconds;
+          Light.try_to_pass traffic_light;
+        ] >>< function
+        | `Ok () when !keep_going -> loop seconds new_happenings
+        | `Ok () ->
+          log_happening ~what_happened:(List.rev new_happenings)
+        | `Error e ->
+          Log.(s "System.Sleep Error!!"  @ error);
+          fail (`Failure "System.sleep")
+      end
+    in
+    with_cbreak (fun () ->
+        Deferred_list.for_concurrent ~f:(fun x -> x) [
+          begin loop 2. [] end;
+          begin
+            let rec kbd_loop () =
+              Log.(s "Press the 'q' key to stop loopping." @ normal);
+              get_key ()
+              >>= function
+              | 'q' | 'Q' ->
+                keep_going := false;
+                Light.green traffic_light;
+                return ()
+              | _ -> kbd_loop ()
+            in
+            kbd_loop ()
+          end;
+        ])
+    >>= fun ((_ : unit list), errors) ->
+    begin match errors with
+    | [] -> return ()
+    | some -> 
+      Log.(s "Errors: "
+           % OCaml.list (fun e -> s (Error.to_string e)) some @ error);
+      fail (`Failure "run")
+    end
   | sl -> 
     Log.(s "Unknown state-running command: " % OCaml.list (sf "%S") sl
          @ error);
     fail (`Wrong_command_line sl)
   end
-
-(** [with_cbreak f] calls with the terminal in “get key” mode. 
-         It comes from
-         http://pleac.sourceforge.net/pleac_ocaml/userinterfaces.html
-*)
-let with_cbreak (f: unit -> (_, _) t) =
-  let open Lwt_unix in
-  Lwt.(tcgetattr stdin
-       >>= fun term_init ->
-       let term_cbreak = { term_init with c_icanon = false } in
-       tcsetattr stdin TCSANOW term_cbreak
-       >>= fun () ->
-       catch f (fun exn -> return (`Error (`Failure "with_cbreak")))
-       >>= fun res ->
-       tcsetattr stdin TCSADRAIN term_init
-       >>= fun () ->
-       return res)
-
-let get_key () =
-  with_cbreak (fun () ->
-      wrap_deferred (fun () -> Lwt_io.read_char Lwt_io.stdin)
-        ~on_exn:(fun e -> (`Failure "get_key")))
 
 let kill ~state ~interactive ids =
   begin 
@@ -236,53 +290,65 @@ let cmdliner_main ?plugins ?override_configuration ?argv ?additional_term () =
                  & info ["-F"; "item-format"] ~docv:"FORMAT-STRING"
                    ~doc:"Use $(docv) as format for displaying jobs")
         ) in
-    let run_cmd =
-      let open Term in
-      sub_command
-        ~term:(
-          pure (fun config_path how ->
-              Configuration.get_configuration ?override_configuration config_path
-              >>= fun configuration ->
-              with_state ?plugins ~configuration (run_state ~how))
-          $ config_file_argument
-          $ Arg.(non_empty @@ pos_all string [] @@
-                 info [] ~docv:"HOW" ~doc:"Use $(docv) method: `step`"))
-        ~info:(
-          info "run" ~version ~sdocs:"COMMON OPTIONS" 
-            ~doc:"Run steps of the engine." 
-            ~man:[])
-    in
-    let kill_cmd =
-      let open Term in
-      sub_command
-        ~term:(
-          pure (fun config_path interactive ids ->
-              Configuration.get_configuration ?override_configuration config_path
-              >>= fun configuration ->
-              with_state ?plugins ~configuration 
-                (kill ~interactive ids))
-          $ config_file_argument
-          $ Arg.(value & flag & info ["i"; "interactive"] 
-                   ~doc:"Go through running targets and kill them with 'y' \
-                         or 'n'.")
-          $ Arg.(value @@ pos_all string [] @@
-                 info [] ~docv:"Target-Id" ~doc:"Kill target $(docv)"))
-        ~info:(
-          info "kill" ~version ~sdocs:"COMMON OPTIONS" 
-            ~doc:"Kill a target." 
-            ~man:[])
-    in
-    let default_cmd = 
-      let doc = "A Workflow Engine for Complex Experimental Workflows" in 
-      let man = [] in
-      sub_command
-        ~term:Term.(ret (pure (`Help (`Plain, None))))
-        ~info:(Term.info "ketrew" ~version ~doc ~man) in
-    let cmds = [init_cmd; info_cmd; run_cmd; kill_cmd] in
-    match Term.eval_choice ?argv default_cmd cmds with
-    | `Ok f -> f
-    | `Error _ -> exit Return_code.cmdliner_error
-    | `Version | `Help -> exit 0
+  let run_cmd =
+    let open Term in
+    sub_command
+      ~term:(
+        pure (fun config_path max_sleep how ->
+            Configuration.get_configuration ?override_configuration config_path
+            >>= fun configuration ->
+            with_state ?plugins ~configuration (run_state ~max_sleep ~how))
+        $ config_file_argument
+        $ Arg.(value & opt float 60.
+               & info ["max-sleep"] ~docv:"SECONDS" 
+                 ~doc:"Maximal sleep time between 2 steps (applies to `loop`)")
+        $ Arg.(non_empty @@ pos_all string [] @@
+               info [] ~docv:"HOW" 
+                 ~doc:"Tell Ketrew to run in a given mode (see below)")
+      )
+      ~info:(
+        let man = [
+          `S "The HOW argument";
+          `P "The following “run” methods are available:";
+          `I ("`step`", "run one single step"); `Noblank;
+          `I ("`fix`", "run  steps until nothing new happens"); `Noblank;
+          `I ("`loop`", "loop `fix` until pressing 'q' (there is a \
+                        timed-wait starting at 2 seconds until `--max-sleep`)")
+        ] in
+        info "run" ~version ~sdocs:"COMMON OPTIONS" 
+          ~doc:"Run steps of the engine."  ~man)
+  in
+  let kill_cmd =
+    let open Term in
+    sub_command
+      ~term:(
+        pure (fun config_path interactive ids ->
+            Configuration.get_configuration ?override_configuration config_path
+            >>= fun configuration ->
+            with_state ?plugins ~configuration 
+              (kill ~interactive ids))
+        $ config_file_argument
+        $ Arg.(value & flag & info ["i"; "interactive"] 
+                 ~doc:"Go through running targets and kill them with 'y' \
+                       or 'n'.")
+        $ Arg.(value @@ pos_all string [] @@
+               info [] ~docv:"Target-Id" ~doc:"Kill target $(docv)"))
+      ~info:(
+        info "kill" ~version ~sdocs:"COMMON OPTIONS" 
+          ~doc:"Kill a target." 
+          ~man:[])
+  in
+  let default_cmd = 
+    let doc = "A Workflow Engine for Complex Experimental Workflows" in 
+    let man = [] in
+    sub_command
+      ~term:Term.(ret (pure (`Help (`Plain, None))))
+      ~info:(Term.info "ketrew" ~version ~doc ~man) in
+  let cmds = [init_cmd; info_cmd; run_cmd; kill_cmd] in
+  match Term.eval_choice ?argv default_cmd cmds with
+  | `Ok f -> f
+  | `Error _ -> exit Return_code.cmdliner_error
+  | `Version | `Help -> exit 0
 
 
 let run_main ?plugins ?argv ?override_configuration () =
