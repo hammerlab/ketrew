6336a13ea950d55f39c41328fe53bc02ca392416
Sebastien Mondet <seb@mondet.org>
2014-06-26 18:20:02 -0400

    lib: use `activated_by_user` as a target-filter

--- src/lib/ketrew_command_line.ml
+++ src/lib/ketrew_command_line.ml
@@ -450,6 +450,8 @@ module Explorer = struct
     filter Target.Is.failed     ~char:'t' ~log:Log.(s "Terminated, success or failure");
     filter Target.Is.finished   ~char:'f' ~log:Log.(s "Failed");
     filter Target.Is.successful ~char:'s' ~log:Log.(s "Successful");
+    filter Target.Is.activated_by_user ~char:'u'
+      ~log:Log.(s "Activated by user (i.e. not as dependency)");
   ]
 
   let get_filter () =
