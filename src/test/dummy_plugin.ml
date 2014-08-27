fb00d16c4251a991df591bcd487d61f5cf396e55
Sebastien Mondet <seb@mondet.org>
2014-08-27 18:35:25 -0400

    test: add a dumb additional query to the dummy-plugin

--- src/test/dummy_plugin.ml
+++ src/test/dummy_plugin.ml
@@ -1,4 +1,6 @@
 
+open Ketrew_pervasives
+
 let name = "dummy"
 
 let create ~host program =
@@ -9,6 +11,22 @@ let create ~host program =
 module Another_long_running : Ketrew_long_running.LONG_RUNNING = struct
   include Ketrew_daemonize
   let name = name
+
+  let additional_queries run_param =
+    ("date", Log.(s "Display the date, not even on the right host"))
+    :: additional_queries run_param
+
+  let query run_param item =
+    if item = "date" then
+      begin Ketrew_host.(get_shell_command_output (of_string "/tmp")
+                           (fmt "date"))
+        >>< function
+        | `Ok (o, _) -> return o
+        | `Error e ->
+          fail Log.(s "Command `date` failed: " % s (Ketrew_error.to_string e))
+      end
+    else (* call Ketrew_daemonize's function: *)
+      query run_param item
 end
 
 let () =
