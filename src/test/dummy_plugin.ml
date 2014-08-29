ca21e94c8f11e0d5e111bdb6db3540ed56fc6dd7
Sebastien Mondet <seb@mondet.org>
2014-08-29 14:46:04 -0400

    doc: document test `dummy_plugin.ml`

--- src/test/dummy_plugin.ml
+++ src/test/dummy_plugin.ml
@@ -1,12 +1,39 @@
 
+(*M
+
+Dummy Plugin
+============
+
+This plugin “inherits” from the implementation of `Ketrew_daemonize` and adds a
+new “query” (that just retrives the result of the `date` command; pretty
+useless).
+
+M*)
 open Ketrew_pervasives
 
+(*M
+
+The name has to be “unique”; the `create` function calls
+`Ketrew_daemonize.create` and changes the `name`:
+
+M*)
 let name = "dummy"
 
 let create ~host program =
   let `Long_running  (_, serialized) =
     Ketrew_daemonize.create ~using:`Python_daemon ~host program in
   `Long_running (name, serialized)
+
+(*M
+Implementation
+--------------
+
+The module `Another_long_running` is the actual implementation of the plugin.
+
+The functions `additional_queries` and `query` deal with the `"date"` query,
+or pass the baby to the `Ketrew_daemonize` module.
+
+M*)
 
 module Another_long_running : Ketrew_long_running.LONG_RUNNING = struct
   include Ketrew_daemonize
@@ -29,6 +56,12 @@ module Another_long_running : Ketrew_long_running.LONG_RUNNING = struct
       query run_param item
 end
 
+(*M
+Registration
+------------
+  
+Registering the plugin is a simple function call using first class modules:
+M*)
 let () =
   Ketrew_state.register_long_running_plugin ~name (module Another_long_running)
 
