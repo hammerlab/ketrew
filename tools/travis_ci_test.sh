da3d0e41fd6524c3c8905522393dbbf4b388f5ff
Sebastien Mondet <seb@mondet.org>
2015-02-20 12:29:09 -0500

    Fix Travis (sqlite)
    The source of the failures was the version of Sqlite on Linux hosts that
    broke Trakeva assumptions (now the travis build, also builds and runs the tests
    of Trakeva).
    A lot of other changes were made while attempting to fix this.

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -20,6 +20,15 @@ travis_install_on_linux () {
 
     echo "yes" | sudo add-apt-repository ppa:$ppa
     sudo apt-get update -qq
+
+    # Hack around Sqlite: https://github.com/cmu-is-projects/ferry/blob/master/.travis.yml
+    sudo apt-get autoremove sqlite3
+    sudo apt-add-repository -y ppa:travis-ci/sqlite3
+    sudo apt-get -y update
+    sudo apt-cache show sqlite3
+    sudo apt-get install sqlite3=3.7.15.1-1~travis1
+    sudo sqlite3 -version
+
     sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time git
 }
 
@@ -85,6 +94,18 @@ ocamlfind list | grep lwt
 echo 'ocamlfind list | grep cohttp'
 ocamlfind list | grep cohttp
 
+echo "Sqlite version"
+sqlite3 -version
+
+echo "Test-trakeva"
+git clone  git://github.com/smondet/trakeva
+cd trakeva
+make configure
+make
+./trakeva_tests
+cd ..
+
+echo "Now build Ketrew + tests"
 
 make gen
 make configure
@@ -92,6 +113,10 @@ make
 
 echo "Mini-test:"
 ./ketrew-test config-file
+
+echo "Graph-test:"
+./ketrew-test automaton-graph
+
 
 echo "Test-env:"
 make test-env
@@ -102,8 +127,9 @@ shopt -s expand_aliases
 echo "Kdserver"
 kdserver --help=plain
 
-ksplugin_user "ls -l $HOME"
+ksplugin_user "sleep 2"
 kscli run loop > kscli_01.out
+
 
 echo "Output file kscli_01.out"
 tail -n 50 kscli_01.out
