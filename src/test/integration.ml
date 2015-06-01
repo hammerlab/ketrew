a248cb4f9aea23821352b7281b7373861aa1e1e2
Sebastien Mondet <seb@mondet.org>
2015-06-01 17:59:00 -0400

    Remove double license header in `intergration.ml`

--- src/test/integration.ml
+++ src/test/integration.ml
@@ -25,22 +25,6 @@ Big Integration Test
 
 M*)
 
-(**************************************************************************)
-(*  Copyright 2014, Sebastien Mondet <seb@mondet.org>                     *)
-(*                                                                        *)
-(*  Licensed under the Apache License, Version 2.0 (the "License");       *)
-(*  you may not use this file except in compliance with the License.      *)
-(*  You may obtain a copy of the License at                               *)
-(*                                                                        *)
-(*      http://www.apache.org/licenses/LICENSE-2.0                        *)
-(*                                                                        *)
-(*  Unless required by applicable law or agreed to in writing, software   *)
-(*  distributed under the License is distributed on an "AS IS" BASIS,     *)
-(*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or       *)
-(*  implied.  See the License for the specific language governing         *)
-(*  permissions and limitations under the License.                        *)
-(**************************************************************************)
-
 open Nonstd
 let say fmt = ksprintf (fun s -> printf "%s\n%!" s) fmt
 let (//) = Filename.concat
