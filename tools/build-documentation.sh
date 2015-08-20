a82340cb8700b68681722b72482eee0bed8e68bc
Sebastien Mondet <seb@mondet.org>
2015-08-20 13:17:02 -0400

    Fix the build of the documentation
    This implements a wonderful hack around `ocamldoc`'s lack of support for packed
    modules.

--- tools/build-documentation.sh
+++ tools/build-documentation.sh
@@ -18,21 +18,45 @@ make_doc () {
     fi
     local apidoc=_apidoc/$git_branch
     if [ "$KAPI_DOC" != "no" ] ; then
-        mkdir -p $apidoc
-        pure_files=`find src/pure -type f`
-        lib_files=`find src/lib -type f`
+        mkdir -p $apidoc/src/
+        #pure_files=`find src/pure -type f -name '*.mli'`
+        pure_mlis=`ocamldep -one-line -modules src/pure/*.mli | cut -d : -f 1`
+        lib_mlis=`ocamldep -one-line -modules src/lib/*.mli | cut -d : -f 1`
+        local out_pure=$apidoc/src/ketrew_pure.mli
+        echo "(** The “pure” common library (mostly data) *)" > $out_pure
+        for i in $pure_mlis ; do
+            base=`basename $i`
+            basebase=${base%.mli}
+            echo "module ${basebase^}"
+            echo "module ${basebase^} : sig" >> $out_pure
+            cat $i >> $out_pure
+            echo "end" >> $out_pure
+        done
+        local out_lib=$apidoc/src/ketrew.mli
+        echo "(** The library that actually does things in a UNIX environment (contains the engine and the server) *)" > $out_lib
+        for i in $lib_mlis ; do
+            base=`basename $i`
+            basebase=${base%.mli}
+            echo "module ${basebase^}"
+            echo "module ${basebase^} : sig" >> $out_lib
+            cat $i >> $out_lib
+            echo "end" >> $out_lib
+        done
+        echo "module Long_running : sig (* manual one *)" >> $out_lib
+        cat src/lib/long_running.ml >> $out_lib
+        echo "end" >> $out_lib
         ocamlfind ocamldoc -html -d $apidoc -package $ocamlfind_packages  -thread \
-                  -charset UTF-8 -t "Ketrew API" -keep-code -colorize-code -sort \
+                  -charset UTF-8 -t "Ketrew API" -keep-code -colorize-code \
                   -I _build/src/lib/  -I _build/src/pure \
-                  $pure_files $lib_files
+                  $out_pure $out_lib
     fi
 
-    INPUT=src/doc/,src/test/ \
+    INPUT=src/doc/,src/test/Workflow_Examples.ml,src/test/dummy_plugin_user.ml,src/test/dummy-plugin \
          INDEX=README.md \
          TITLE_PREFIX="Ketrew: " \
          API=$apidoc/ \
          COMMAND_SUBSTITUTIONS=ketrew:./ketrew \
-         CATCH_MODULE_PATHS='^Ketrew[_a-z]*:,LONG_RUNNING:Ketrew_long_running.' \
+         CATCH_MODULE_PATHS='^Ketrew.:,LONG_RUNNING:Ketrew.Long_running.' \
          OUTPUT_DIR=$outdir \
          TITLE_SUBSTITUTIONS="dummy_plugin:Plugin Example, \
                          dummy_plugin_user:Plugin-Usage Example, \
