5eb8d272f81dfeb1fd2987711f0218c7aa6f6350
Sebastien Mondet <seb@mondet.org>
2016-09-07 15:55:02 -0400

    Add `4.03.0` to the Travis build

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -2,24 +2,9 @@
 
 
 
-export opam_pin_add=""
 travis_install_on_linux () {
-    # Install OCaml and OPAM PPAs
-    case "$OCAML_VERSION,$OPAM_VERSION" in
-        3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-        3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-        4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-        4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-        4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-        4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-        4.01.0,1.2.0) ppa=avsm/ocaml41+opam12; export opam_pin_add="add" ;;
-        4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-        4.02.0,1.2.0) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
-        4.02.2,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-        4.02.2,1.2.0) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
-        4.02.3,1.2.1) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
-      *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-    esac
+    # Install OPAM PPA
+    export ppa=avsm/ocaml42+opam12
 
     echo "yes" | sudo add-apt-repository ppa:$ppa
     sudo apt-get update -qq
@@ -56,7 +41,6 @@ travis_install_on_osx () {
     echo "/usr/local/Cellar/sqlite/:"
     ls -l /usr/local/Cellar/sqlite/
     export opam_init_options="--comp=$OCAML_VERSION"
-    export opam_pin_add="add"
 }
 
 
@@ -68,8 +52,6 @@ esac
 
 # configure and view settings
 export OPAMYES=1
-echo "ocaml -version"
-ocaml -version
 echo "opam --version"
 opam --version
 echo "git --version"
@@ -79,15 +61,21 @@ git --version
 opam init $opam_init_options
 eval `opam config env`
 
+echo "ocaml -version"
+ocaml -version
+
 # Bypass opam bug #1747
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
 opam update
 
+# Cf. https://github.com/mirleft/ocaml-nocrypto/issues/104
+opam pin add oasis 0.4.6
+
 opam install sqlite3 postgresql
 
-opam pin $opam_pin_add ketrew .
+opam pin add ketrew .
 
 opam install --yes ketrew
 
