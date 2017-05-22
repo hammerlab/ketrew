92a8be77d73fa8494a619ded1109957c0524ac62
Sebastien Mondet <seb@mondet.org>
2017-05-22 15:45:01 -0400

    Fix docker-compose demo-setup
    This is the output from
    PR [`hammerlab/secotrec#57`](https://github.com/hammerlab/secotrec/pull/57).
    It fixes #534.

--- tools/docker-compose/kdc.sh
+++ tools/docker-compose/kdc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export kdc='docker-compose --file ./tools/docker-compose/secotrec-local.yml'
+export kdc='docker-compose --file ./tools/docker-compose/docker-compose.json'
 
 mkdir -p /tmp/secotrec-local-shared-temp
 chmod -R 777 /tmp/secotrec-local-shared-temp
